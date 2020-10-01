fe0714e5d323e653f98ece39b80cad5c53c947ca
fwase <fylypexp@gmail.com>
2020-09-30 22:16:14 -0400

    Add more functions

--- activity_2/activity_2.py
+++ activity_2/activity_2.py
@@ -28,6 +28,40 @@ def confidence_interval(sample_values, confidence_level):
         
         return (min_value, max_value)
 
+def mean_zero_test(conf_interval):
+    min_value, max_value = conf_interval
+    if min_value <= 0 <= max_value:
+        return True
+    
+    return False
+
+def confidence_interval_unpaired_samples(sample_values_a, sample_values_b, confidence_level):
+    mean_sample_a = arithmetic_average(sample_values_a)
+    mean_sample_b = arithmetic_average(sample_values_b)
+    diff_mean_samples = mean_sample_a - mean_sample_b
+    
+    std_dev_sample_a = std_dev(sample_values_a)
+    std_dev_sample_b = std_dev(sample_values_b)
+    diff_mean_std_dev = sqrt((std_dev_sample_a / mean_sample_a) + (std_dev_sample_b / mean_sample_b))
+    
+    numerator_df = ((std_dev_sample_a / mean_sample_a) + (std_dev_sample_b / mean_sample_b)) ** 2
+    denominator_df_sample_a = (1 / (mean_sample_a - 1)) * ((std_dev_sample_a / mean_sample_a) ** 2)
+    denominator_df_sample_b = (1 / (mean_sample_b - 1)) * ((std_dev_sample_b / mean_sample_b) ** 2)
+    denominator_df = denominator_df_sample_a + denominator_df_sample_b
+    degrees_freedom = round((numerator_df / denominator_df) - 2)
+    
+    z_value = t.ppf(confidence_level, degrees_freedom)
+    min_value = diff_mean_samples - (diff_mean_std_dev * z_value)
+    min_value = diff_mean_samples + (diff_mean_std_dev * z_value)
+    
+    return (min_value, min_value)
+
+def size_of_sample(mean, std_dev_value, z_value, precision):
+    numerator = 100 * std_dev_value  * z_value
+    denominator = precision * mean
+    
+    return (numerator / denominator) ** 2
+
 if __name__ == '__main__':
     values = [-0.04,-0.19, 0.14, -0.09, -0.14, 0.19, 0.04, 0.09]
     print(confidence_interval(values, 0.95))
